f28b618705134df72bba4e76a61c1cd2d4bf7b2b
Pashkovskaya Elizaveta <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-11-02 17:04:18 +0200

    add modal window

--- mainScreen/main.js
+++ mainScreen/main.js
@@ -1,15 +1,13 @@
-
-
-/* const addMoreBtn = document.querySelector('.view_more_btn')
-addMoreBtn.addEventListener('click', () => {
-    const gallery2 = document.querySelector('.gallery2')
-    gallery2.style.display = "grid"
-}) */
-
-function viewMore()
-{
-    const gallery2 = document.querySelector('.gallery2')
-    gallery2.style.display = "grid"
+function viewMore() {
+  const gallery2 = document.querySelector(".gallery2");
+  gallery2.style.display = "grid";
 }
-
-
+function sendMessageModal() {
+  const dialog = document.querySelector("#dialog");
+  dialog.showModal();
+}
+function closeModal()
+{
+    const closeModal = document.querySelector('#clodeModal')
+    closeModal.closeModal()
+}
