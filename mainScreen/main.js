c6a3214fe4bc9e5b83c0cf7c0ed92f43fdb45599
Pashkovskaya Elizaveta <s2200704@BC-C02J64A8Q6LR-MacBook-Air.local>
2022-11-02 09:59:51 +0200

    add main screen

---
+++ mainScreen/main.js
@@ -0,0 +1,15 @@
+
+
+/* const addMoreBtn = document.querySelector('.view_more_btn')
+addMoreBtn.addEventListener('click', () => {
+    const gallery2 = document.querySelector('.gallery2')
+    gallery2.style.display = "grid"
+}) */
+
+function viewMore()
+{
+    const gallery2 = document.querySelector('.gallery2')
+    gallery2.style.display = "grid"
+}
+
+
